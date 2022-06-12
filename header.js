009fd6d2ce9a77e4e92b87312bc6088d42a5401c
gobernacion.archivo.central@gmail.com <gobernacion.archivo.central@gmail.com>
2022-06-11 21:23:56 -0500

    final UX UI

--- header.js
+++
@@ -1,7 +0,0 @@
-const navToggle = document.querySelector(".toggle")
-const navMenu = document.querySelector(".navbar")
-
-
-navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("nav-menu_visible");
-});
